55b9ab72eda506384e6677149b2d5276dea3afb2
galqawala <galq+github@protonmail.ch>
2020-11-23 17:41:48 +0200

    first tilemap

---
+++ Assets/tiles.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class tiles : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public Tile[] tilesToPlace;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int radius = 100;
+        float scale = 1f/10f;
+        for (int x = -radius; x <= radius; x++)
+        {
+            for (int y = -radius; y <= radius; y++)
+            {
+                Vector3Int p = new Vector3Int(x,y,0);
+                float noiseX = (x+10000f)*scale;
+                float noiseY = (y+10000f)*scale;
+                float noiseValue = Mathf.PerlinNoise(noiseX,noiseY);
+                int tileIndex = (int)(Mathf.Round(noiseValue));
+                // print(x+" , "+y+" / "+noiseX+" , "+noiseY+": "+Mathf.PerlinNoise(noiseX,noiseY)+" -> "+tileIndex);
+                tilemap.SetTile(p, tilesToPlace[tileIndex]);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
