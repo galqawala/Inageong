9d8648e4994a0bc1aff5b79df8256bf0b41a7c67
galqawala <galq+github@protonmail.ch>
2020-11-29 20:16:19 +0200

    Snow & ice

--- Assets/tiles.cs
+++ Assets/tiles.cs
@@ -8,7 +8,7 @@ using UnityEngine.Tilemaps;
 public class tiles : MonoBehaviour
 {
     public Grid grid;
-    public Tilemap tilemap;
+    public Tilemap[] terrainLayers;
     public int spawnRadius = 8;
 
     List<Tile> tileList = new List<Tile>();
@@ -16,7 +16,7 @@ public class tiles : MonoBehaviour
     enum Material {
         Unidentified, Transparent,
         //from bottom to top (first one will be rendered below the later ones)
-        Water, Grass
+        Water, Ice, Grass, Snow
     }
 
     class CornersTile {
@@ -31,11 +31,15 @@ public class tiles : MonoBehaviour
         if (color.a <= 0.1f) return Material.Transparent;
         if (rgbAbout(color, 0.302f, 0.573f, 0.384f)) return Material.Grass;
         if (rgbAbout(color, 0.388f, 0.608f, 1.000f)) return Material.Water;
+        if (rgbAbout(color, 0.796f, 0.859f, 0.988f)) return Material.Ice;
+        if (rgbAbout(color, 0.373f, 0.804f, 0.894f)) return Material.Ice;
+        if (rgbAbout(color, 1.000f, 1.000f, 1.000f)) return Material.Snow;
+        print("Unidentified color: "+color+" "+htmlColor(color));
         return Material.Unidentified;
     }
 
     bool about(float a, float b) {
-        float margin = 0.001f;
+        float margin = 0.06f;
         return a >= b-margin && a <= b+margin;
     }
 
@@ -50,45 +54,79 @@ public class tiles : MonoBehaviour
         return "#"+r.ToString("X")+g.ToString("X")+b.ToString("X");
     }
 
-    Tile tileByCornerMats(Material tr, Material br, Material bl, Material tl) {
+    List<Tile> tileByCornerMats(Material tr, Material br, Material bl, Material tl) {
+        List<Tile> tileLayers = new List<Tile>(); //from bottom to top
         var validTiles = tileCornerList.Where( t =>
             t.topRight==tr && t.bottomRight==br && t.bottomLeft==bl && t.topLeft==tl
         );
         print(validTiles.Count()+" tiles found for: ⌝"+tr+" ⌟"+br+" ⌞"+bl+" ⌜"+tl);
         if (validTiles.Count() > 0) {
-            return validTiles.OrderBy(qu => System.Guid.NewGuid()).First().tile;
+            tileLayers.Add(validTiles.OrderBy(qu => System.Guid.NewGuid()).First().tile);
+        } else {
+            //no such tile -> use layers and transparency to build the required combination
+            uint layerMask = 0b_1111; //opaque corners: ⌝⌟⌞⌜
+            var materials = Enum.GetValues(typeof(Material)).Cast<Material>();
+            foreach (var mat in materials)
+            {
+                if (mat==tr || mat==br || mat==bl || mat==tl) {
+                    validTiles = tileCornerList.Where( t =>
+                            t.topRight    == ((layerMask&0b_1000)>0 ? mat : Material.Transparent)
+                        &&  t.bottomRight == ((layerMask&0b_0100)>0 ? mat : Material.Transparent)
+                        &&  t.bottomLeft  == ((layerMask&0b_0010)>0 ? mat : Material.Transparent)
+                        &&  t.topLeft     == ((layerMask&0b_0001)>0 ? mat : Material.Transparent)
+                    );
+                    tileLayers.Add(validTiles.OrderBy(qu => System.Guid.NewGuid()).First().tile);
+                    //remove correctly placed corners from the mask
+                    layerMask &= ~(
+                         (uint)((tr==mat) ? 0b_1000 : 0b_0000)
+                        |(uint)((br==mat) ? 0b_0100 : 0b_0000)
+                        |(uint)((bl==mat) ? 0b_0010 : 0b_0000)
+                        |(uint)((tl==mat) ? 0b_0001 : 0b_0000)
+                    );
+                }
+                if (layerMask==0) return tileLayers;
+            }
         }
-        return tileCornerList.OrderBy(qu => System.Guid.NewGuid()).First().tile;
+        return tileLayers;
     }
 
     void Start() {
         var size = 32;
-        Texture2D tileset = Resources.Load<Texture2D>("RPG Nature Tileset") as Texture2D;
-        for (int x = 0; x+size <= tileset.width; x+=size)
+        List<string> tilesetNames = new List<string>() {
+            "RPG Nature Tileset","IceTileset"
+        };
+
+        foreach (var tilesetName in tilesetNames)
         {
-            for (int y = 0; y+size <= tileset.height; y+=size)
+            Texture2D tileset = Resources.Load<Texture2D>(tilesetName) as Texture2D;
+            for (int x = 0; x+size <= tileset.width; x+=size)
             {
-                // if (x!=64 || y!=128) continue;
-                Tile tile = ScriptableObject.CreateInstance<Tile>();
-                tile.sprite = Sprite.Create(
-                    tileset,
-                    new Rect(x, y, size, size), // section of texture to use
-                    new Vector2(0.5f, 0.5f), // pivot in centre
-                    size, // pixels per unity tile grid unit
-                    1,
-                    SpriteMeshType.Tight,
-                    Vector4.zero
-                );
-                tileList.Add(tile);
-                CornersTile cornersTile =
-                    new CornersTile {
-                        topRight    = materialByColor(tileset.GetPixel(x+size-1,y))
-                    ,   bottomRight = materialByColor(tileset.GetPixel(x+size-1,y+size-1))
-                    ,   bottomLeft  = materialByColor(tileset.GetPixel(x,y+size-1))
-                    ,   topLeft     = materialByColor(tileset.GetPixel(x,y))
-                    ,   tile        = tile
-                    };
-                tileCornerList.Add(cornersTile);
+                for (int y = 0; y+size <= tileset.height; y+=size)
+                {
+                    // if (x!=256 || y!=160) continue;
+                    Tile tile = ScriptableObject.CreateInstance<Tile>();
+                    tile.sprite = Sprite.Create(
+                        tileset,
+                        new Rect(x, y, size, size), // section of texture to use
+                        new Vector2(0.5f, 0.5f), // pivot in centre
+                        size, // pixels per unity tile grid unit
+                        1,
+                        SpriteMeshType.Tight,
+                        Vector4.zero
+                    );
+                    tileList.Add(tile);
+                    CornersTile cornersTile =
+                        new CornersTile {
+                            topRight    = materialByColor(tileset.GetPixel(x+size-1,y))
+                        ,   bottomRight = materialByColor(tileset.GetPixel(x+size-1,y+size-1))
+                        ,   bottomLeft  = materialByColor(tileset.GetPixel(x,y+size-1))
+                        ,   topLeft     = materialByColor(tileset.GetPixel(x,y))
+                        ,   tile        = tile
+                        };
+                    tileCornerList.Add(cornersTile);
+                    print("⌝"+cornersTile.topRight+" ⌟"+cornersTile.bottomRight
+                        +" ⌞"+cornersTile.bottomLeft+" ⌜"+cornersTile.topLeft+" loaded from "+tilesetName);
+                }
             }
         }
     }
@@ -101,14 +139,20 @@ public class tiles : MonoBehaviour
             for (int y = centerCell.y-spawnRadius; y <= centerCell.y+spawnRadius; y++)
             {
                 Vector3Int coords = new Vector3Int(x,y,0);
-                if (!tilemap.GetTile(coords)) {
-                    tilemap.SetTile(coords, getTileFor(x,y));
+                var layer = 0;
+                if (!terrainLayers[layer].GetTile(coords)) {
+                    var tiles = getTileFor(x,y);
+                    foreach (var tile in tiles)
+                    {
+                        terrainLayers[layer].SetTile(coords,tile);
+                        layer++;
+                    }
                 }
             }
         }
     }
 
-    Tile getTileFor(int x, int y)
+    List<Tile> getTileFor(int x, int y)
     {
         // think of the tile actually being in the intersection of 4 coordinates
         return tileByCornerMats(
@@ -121,10 +165,11 @@ public class tiles : MonoBehaviour
 
     Material getMaterialFor(int x, int y)
     {
+        var temperature = getValue(x, y, 10113f, 10317f, 438.66633850979453f);
         if (isWater(x,y)) {
-            return Material.Water;
+            return temperature>0.5f ? Material.Water : Material.Ice;
         } else {
-            return Material.Grass;
+            return temperature>0.5f ? Material.Grass : Material.Snow;
         }
     }
 
